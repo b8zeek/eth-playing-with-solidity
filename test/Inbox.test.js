ed872ee54ae5fe4c425eb4d7f1ad35ac6074f703
Mirko Basic <bejzik8@gmail.com>
2022-07-10 20:00:32 +0200

    First commit.

---
+++ test/Inbox.test.js
@@ -0,0 +1,32 @@
+// contract test code will go here
+const assert = require('assert')
+const ganache = require('ganache-cli')
+const Web3 = require('web3')
+const web3 = new Web3(ganache.provider())
+
+const { interface, bytecode } = require('../compile')
+
+let accounts
+let inbox
+
+beforeEach(async () => {
+    // get a list of all accounts
+    accounts = await web3.eth.getAccounts()
+
+    // use one of those accounts to deploy a contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({
+            data: bytecode,
+            arguments: ['Hi there!']
+        })
+        .send({
+            from: accounts[0],
+            gas: '1000000'
+        })
+})
+
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+        assert.ok(inbox.options.address)
+    })
+})
