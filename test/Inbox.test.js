f84587b0f91f9b867fda5b28fe8b6b937644b3bb
Mirko Basic <bejzik8@gmail.com>
2022-07-10 20:27:26 +0200

    Added test case for checking if the contract message can be properly changed.

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -33,6 +33,19 @@ describe('Inbox', () => {
 
     it('has a default message', async () => {
         const message = await inbox.methods.message().call()
+
         assert.equal(message, INITIAL_STRING)
+    })
+
+    it('can change the message', async () => {
+        const newMessage = 'Bye!'
+
+        await inbox.methods.setMessage(newMessage).send({
+            from: accounts[0],
+            gas: '1000000'
+        })
+        const message = await inbox.methods.message().call()
+
+        assert.equal(message, newMessage)
     })
 })
